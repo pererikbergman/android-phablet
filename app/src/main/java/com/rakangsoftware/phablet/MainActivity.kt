c7d28468633bbb22e71c2f20489bcb8e99496b57
Per-Erik Bergman <bergman@uncle.se>
2021-04-18 22:34:44 +0700

    Added phablet code.

---
+++ app/src/main/java/com/rakangsoftware/phablet/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rakangsoftware.phablet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_layout_with_bar)
+    }
+}
